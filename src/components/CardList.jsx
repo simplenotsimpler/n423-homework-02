3abb5690f3e514185f307f7c13c6c5bc45aef713
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-04-15 22:48:44 -0400

    fix: move li tag to CardList

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,17 +1,18 @@
 import CardListStyles from "../styles/CardList.module.css";
 import Card from "./Card.jsx";
 
-//TODO: move the li here
 const CardList = ({ data, detailPath }) => {
   const CardListItems = data.map((el) => {
     return (
-      <Card
-        key={el.id}
-        title={el.title}
-        text={el.description}
-        imgName={el.imgName}
-        href={detailPath ? `${detailPath}/${el.id}` : undefined}
-      />
+      <li>
+        <Card
+          key={el.id}
+          title={el.title}
+          text={el.description}
+          imgName={el.imgName}
+          href={detailPath ? `${detailPath}/${el.id}` : undefined}
+        />
+      </li>
     );
   });
   return <ul className={CardListStyles.cardList}>{CardListItems}</ul>;
